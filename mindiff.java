30269acb6dc44deab3b9bb85ef351b6dea6ea2dc
sangeeanbumani <sangeepavi143@gmail.com>
2016-09-07 11:59:47 +0530

    Create mindiff.java

---
+++ mindiff.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public static void main(String args[])\
+{
+class mindiff
+{
+Scanner s=new Scanner(System.in);
+DateTime t1=s.next();
+DateTime t2=s.next();
+Minutes min=Minutes.minBetween(t1,t2);
+System.out.println("min");
+}
+}
