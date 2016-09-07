e9ce61fdedd155d235dcfb4b4236ccd65989f66e
sangeeanbumani <sangeepavi143@gmail.com>
2016-09-07 11:43:33 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public static void main(String args[])
+{
+class vowels
+{
+Scanner s=new Scanner(System.in);
+String str=s.nextLine();
+int len;
+String str2;
+len=str.length();
+str2=str1.reverse();
+System.out.println("str2");
+str2.toCharArray();
+for(int i=0;i<len;i++)
+{
+if(charAt(i)=='a'||charAt(i)=='e'||charAt(i)=='i'||charAt(i)=='o'||charAt(i)=='u')
+{
+str2=str2.replace('charAt[i]','');
+}
+}
+System.out.println("str2");
+}
+}
