44925cf7b2135e5a92c58a77c70a4777cc988898
sangeeanbumani <sangeepavi143@gmail.com>
2016-09-07 10:58:35 +0530

    Create a.java

---
+++ a.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public static void main(String args[])
+{
+class a
+{
+Scanner s=new Scanner(System.in);
+String str=s.nextLine();
+int result;
+result=Integer.parseInt(str);
+System.out.println("result");
+}
+}
